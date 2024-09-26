648982535ff3302aafbc3f0731ca77994fa7c930
Faraz Nisar <35908301+faraznisar@users.noreply.github.com>
2024-09-26 19:14:37 +0530

    feat - supertoken initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import { ThirdPartyPreBuiltUI } from 'supertokens-auth-react/recipe/thirdparty/prebuiltui';
+import { EmailPasswordPreBuiltUI } from 'supertokens-auth-react/recipe/emailpassword/prebuiltui';
+import * as reactRouterDom from "react-router-dom";
+import SuperTokens, { SuperTokensWrapper } from 'supertokens-auth-react';
+import ThirdParty, {
+  Github,
+  Google,
+  Facebook,
+  Apple,
+} from 'supertokens-auth-react/recipe/thirdparty';
+import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
+import Session from 'supertokens-auth-react/recipe/session';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+SuperTokens.init({
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+    appName: 'supertoken-with-react',
+    apiDomain: 'http://localhost:5001',
+    websiteDomain: 'http://localhost:5173',
+    apiBasePath: '/auth',
+    websiteBasePath: '/auth',
+  },
+  recipeList: [
+    ThirdParty.init({
+      signInAndUpFeature: {
+        providers: [
+          Github.init(),
+          Google.init(),
+          Facebook.init(),
+          Apple.init(),
+        ],
+      },
+    }),
+    EmailPassword.init(),
+    Session.init(),
+  ],
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return (<SuperTokensWrapper>
+    <BrowserRouter>
+      <Routes>
+        {/*This renders the login UI on the /auth route*/}
+        {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
+          ThirdPartyPreBuiltUI,
+          EmailPasswordPreBuiltUI,
+        ])}
+        <reactRouterDom.Route path="/" element={<>Hello</>}/>
+        {/*Your app routes*/}
+      </Routes>
+    </BrowserRouter>
+  </SuperTokensWrapper>);
 }
 
 export default App
